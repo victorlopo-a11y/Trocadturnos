1735d4ccdedada28ab521821baf31a142bef75f5
victorlopo-a11y <victor.lopo@grupomultilaser.com.br>
2026-01-05 23:53:58 -0300

    update supabase anon key

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://eyixhyugrcyapwvqfyet.supabase.co';
-const supabaseAnonKey = 'sb_publishable_g867NqdWpo6WH-22kvVZ1g_AfhvBd19';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5aXhoeXVncmN5YXB3dnFmeWV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQxMDIwNjYsImV4cCI6MjA3OTY3ODA2Nn0.Cm9YmysjXaEX14yqPPbpGBVquFqpVnd-9M_DZg9kyI0';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
