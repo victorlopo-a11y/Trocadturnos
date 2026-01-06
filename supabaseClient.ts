92bc477ad199a07dfc3c4504fee041cddd4fb01c
victorlopo-a11y <victor.lopo@grupomultilaser.com.br>
2026-01-05 23:25:58 -0300

    fix supabase project url

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,7 +1,6 @@
-
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://qtiplyczwjdmcibmwfzg.supabase.co';
+const supabaseUrl = 'https://eyixhyugrcyapwvqfyet.supabase.co';
 const supabaseAnonKey = 'sb_publishable_g867NqdWpo6WH-22kvVZ1g_AfhvBd19';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
